fa6856d8495ef41f7f426732ee0eac4af5cb5fae
Bananabob-hp3 <pantharahit97@gmail.com>
2026-08-07 21:15:55 +0530

    Add first day learning text to learning-javascript

---
+++ js_day1/javascript.js
@@ -0,0 +1,23 @@
+console.log("hello from javascript.js");
+//const pi = 3.14;
+//pi = 10;
+
+//console.log(pi); // What will be output?
+//let age = 11;
+//console.log(age); // outputs 11 to the console
+
+//age = 54;
+
+//console.log(age); // what will be output now?
+//let age = 11;
+//console.log(age); // outputs 11 to the console
+
+//age = 54;
+
+//console.log(age); // what will be output now?
+//let firstName = "John";
+//let lastName = "Doe";
+
+//console.log(firstName);
+//console.log(lastName);
+console.log((3 + 2) -76 * (1 + 1))
