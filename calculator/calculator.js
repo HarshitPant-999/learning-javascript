27c85878e53290eba5b7eeba6b6ee1c22a91bea4
Bananabob-hp3 <pantharahit97@gmail.com>
2026-08-14 21:14:45 +0530

    Add Calculator project with Javascript -m Frontend framework css

---
+++ calculator/calculator.js
@@ -0,0 +1,38 @@
+numOne = prompt("Enter a number")
+operator = prompt("Enter an operator")
+numTwo = prompt("Enter a second number")
+
+add = () => {
+  return +;
+};
+
+subtract = () => {
+  return -;
+};
+
+multiply = () => {
+  return *;
+};
+
+divide = () => {
+  return /;
+};
+
+operate = (operator, numOne, numTwo) => {
+  return `${operate}`
+}
+
+const count = 0;
+
+numOne = prompt("Enter a number")
+operator = prompt("Enter an operator")
+numTwo = prompt("Enter a second number")
+
+const count = `${numOne}{add}{numTwo}`
+resetButton = addEventListener("click", () => {
+  count = 0;
+})
+
+
+
+
